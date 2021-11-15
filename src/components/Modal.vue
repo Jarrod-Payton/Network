a455d37368a2eed3031074afba323ec5f6d2a17e
Jarrod-Payton <lucasjpayton101@gmail.com>
2021-11-14 22:41:19 -0800

    Posts on render of profile

---
+++ src/components/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="modal" tabindex="-1" id="profile-modal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ activePost.title }}</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <p>{{ activePost.body }}</p>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { computed } from "@vue/reactivity";
+import { AppState } from "../AppState";
+import { Modal } from "bootstrap";
+
+export default {
+  setup() {
+    return {
+      Account: computed(() => AppState.account),
+      Profile: computed(() => AppState.profile),
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
